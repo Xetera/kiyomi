369114f2515ce8ddb50cd44745d5c6a2655012fe
xetera <mythologyali@gmail.com>
2021-12-11 21:49:01 +0300

    ignoring weird document build errors

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -5,26 +5,24 @@ import Document, {
   Main,
   NextScript,
 } from "next/document"
-import React from "react"
 
+// @ts-ignore
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
+    // @ts-ignore
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
 
   render() {
     return (
-      <Html className="dark">
+      <Html className="dark" lang="en">
         <Head />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href={process.env.NEXT_PUBLIC_BASE_URL_CDN} />
-        <link rel="dns-prefetch" href={process.env.NEXT_PUBLIC_BASE_URL_CDN} />
         <body>
           <Main />
           <NextScript />
