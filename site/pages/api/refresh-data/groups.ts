1b47da74f231699cf447946bebe63e7bef57e262
xetera <mythologyali@gmail.com>
2022-01-29 14:05:05 +0300

    trying to get image workflow to run lol

--- site/pages/api/refresh-data/groups.ts
+++ site/pages/api/refresh-data/groups.ts
@@ -2,7 +2,6 @@ import { NextApiHandler } from "next"
 
 const handler: NextApiHandler = async (req, res) => {
   const a = await req.services.search.fullGroupRefresh()
-  console.log(a)
   res.status(200).send("Refreshed groups")
 }
 
